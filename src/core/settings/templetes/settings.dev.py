1f3f4adb6cd51c82bd7025c2d6fa9a46a4b1b22d
abdullahzulfiqar653 <abdullahzulfiqar653@gmail.com>
2024-01-10 23:59:45 +0500

    logging package added and loging setup

--- src/core/settings/templetes/settings.dev.py
+++ src/core/settings/templetes/settings.dev.py
@@ -1,2 +1,10 @@
 DEBUG = True
 SECRET_KEY = 'skjndckjsdcksc=-0-30923ksckcksc'
+
+LOGGING['formatters']['colored'] = {  # noqa: F821
+    '()': 'colorlog.ColoredFormatter',
+    'format': '%(log_color)s%(asctime)s %(levelname)s %(name)s %(bold_white)s%(message)s',
+}
+LOGGING['loggers']['core']['level'] = 'DEBUG'  # noqa: F821
+LOGGING['handlers']['console']['level'] = 'DEBUG'  # noqa: F821
+LOGGING['handlers']['console']['formatter'] = 'colored'  # noqa: F821
